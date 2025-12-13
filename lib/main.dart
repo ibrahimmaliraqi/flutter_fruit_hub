b59a4af4c7216638e39811c4b3a4869e9e3cf81b
Ibrahim Mohhamed <ibrahom0780@gmail.com>
2025-12-13 16:44:35 +0300

    final ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/services/shared_prefs.dart';
 import 'package:fruit_hub/core/utils/helper_functions/on_generate_route.dart';
 import 'package:fruit_hub/feaatures/splash/presentation/views/splash_view.dart';
 import 'package:fruit_hub/generated/l10n.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefs.init();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
   runApp(const FruitsApp());
 }
 
