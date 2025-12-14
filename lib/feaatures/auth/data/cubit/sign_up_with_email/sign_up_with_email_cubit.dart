413d2b660601f2956cd13c395e0af7a4f7fd7409
Ibrahim Mohhamed <ibrahom0780@gmail.com>
2025-12-14 11:00:46 +0300

    create sign up with email cubit done

--- lib/feaatures/auth/data/cubit/sign_up_with_email/sign_up_with_email_cubit.dart
+++ lib/feaatures/auth/data/cubit/sign_up_with_email/sign_up_with_email_cubit.dart
@@ -1,8 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:fruit_hub/feaatures/auth/data/repos/auth_repos.dart';
 import 'package:meta/meta.dart';
 
 part 'sign_up_with_email_state.dart';
 
 class SignUpWithEmailCubit extends Cubit<SignUpWithEmailState> {
-  SignUpWithEmailCubit() : super(SignUpWithEmailInitial());
+  SignUpWithEmailCubit(this.authRepos) : super(SignUpWithEmailInitial());
+  AuthRepos authRepos;
+  Future<void> signUpWithEmail({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    emit(SignUpWithEmailLoading());
+    final result = await authRepos.signUp(
+      email: email,
+      password: password,
+      name: name,
+    );
+    result.fold(
+      (failure) => emit(SignUpWithEmailFailure(failure)),
+      (success) => emit(SignUpWithEmailSuccess(success)),
+    );
+  }
 }
--- lib/feaatures/auth/data/cubit/sign_up_with_email/sign_up_with_email_state.dart
+++ lib/feaatures/auth/data/cubit/sign_up_with_email/sign_up_with_email_state.dart
@@ -4,3 +4,15 @@ part of 'sign_up_with_email_cubit.dart';
 sealed class SignUpWithEmailState {}
 
 final class SignUpWithEmailInitial extends SignUpWithEmailState {}
+
+final class SignUpWithEmailLoading extends SignUpWithEmailState {}
+
+final class SignUpWithEmailSuccess extends SignUpWithEmailState {
+  final String success;
+  SignUpWithEmailSuccess(this.success);
+}
+
+final class SignUpWithEmailFailure extends SignUpWithEmailState {
+  final String failure;
+  SignUpWithEmailFailure(this.failure);
+}
