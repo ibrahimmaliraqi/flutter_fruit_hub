86509974541cb521bc3bd2834b58ddcd4d547599
Ibrahim Mohhamed <ibrahom0780@gmail.com>
2025-12-14 09:23:24 +0300

    add supabase to project

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,21 @@ import 'package:fruit_hub/core/services/shared_prefs.dart';
 import 'package:fruit_hub/core/utils/helper_functions/on_generate_route.dart';
 import 'package:fruit_hub/feaatures/splash/presentation/views/splash_view.dart';
 import 'package:fruit_hub/generated/l10n.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefs.init();
+  await Supabase.initialize(
+    url: 'https://acbggihxejiapkguvxxz.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjYmdnaWh4ZWppYXBrZ3V2eHh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0Njk4NzcsImV4cCI6MjA4MTA0NTg3N30.HS-GXONR2sFG2S-NCgJvNcSLWBBk2LKTGrjve7WjncA',
+  );
 
   runApp(const FruitsApp());
 }
+
+final supabase = Supabase.instance.client;
 
 class FruitsApp extends StatelessWidget {
   const FruitsApp({super.key});
