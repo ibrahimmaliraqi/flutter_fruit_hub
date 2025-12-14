c8cbe5b580d91a3e071a396366459ed7556557e3
Ibrahim Mohhamed <ibrahom0780@gmail.com>
2025-12-14 11:38:39 +0300

    sign in with email and password logic done

--- lib/feaatures/auth/presentation/widgets/login_view_body.dart
+++ lib/feaatures/auth/presentation/widgets/login_view_body.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_styles.dart';
 import 'package:fruit_hub/core/utils/assets.dart';
+import 'package:fruit_hub/core/utils/helper_functions/snack.dart';
 import 'package:fruit_hub/core/utils/widgets/custom_button.dart';
 import 'package:fruit_hub/core/utils/widgets/custom_text_field.dart';
+import 'package:fruit_hub/feaatures/auth/data/cubit/sign_in_with_email/sign_in_with_email_cubit.dart';
 import 'package:fruit_hub/feaatures/auth/presentation/views/sign_up_view.dart';
 import 'package:fruit_hub/feaatures/auth/presentation/widgets/custom_social_auth_button.dart';
 import 'package:gap/gap.dart';
@@ -18,87 +22,121 @@ class LoginViewBody extends StatefulWidget {
 class _LoginViewBodyState extends State<LoginViewBody> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 17),
-        child: Column(
-          children: [
-            Gap(10),
-            CustomTextField(
-              text: "البريد الإلكتروني",
-              controller: email,
-            ),
-            Gap(16),
-            CustomTextField(
-              isPassword: true,
-              text: "كلمة المرور",
-              controller: password,
-            ),
-            Gap(16),
-            Align(
-              alignment: AlignmentGeometry.centerLeft,
-              child: GestureDetector(
-                child: Text(
-                  "نسيت كلمة المرور؟",
-                  style: AppStyles.semiBold13,
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Gap(10),
+              CustomTextField(
+                text: "البريد الإلكتروني",
+                controller: email,
+              ),
+              Gap(16),
+              CustomTextField(
+                isPassword: true,
+                text: "كلمة المرور",
+                controller: password,
+              ),
+              Gap(16),
+              Align(
+                alignment: AlignmentGeometry.centerLeft,
+                child: GestureDetector(
+                  child: Text(
+                    "نسيت كلمة المرور؟",
+                    style: AppStyles.semiBold13,
+                  ),
                 ),
               ),
-            ),
-            Gap(33),
-            CustomButton(text: "تسجيل دخول"),
-            Gap(33),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "لا تمتلك حساب؟ ",
-                  style: AppStyles.semiBold16,
-                ),
-                GestureDetector(
-                  onTap: () => Navigator.of(context).pushNamed(SignUpView.id),
-                  child: Text(
-                    "قم بإنشاء حساب",
-                    style: AppStyles.semiBold16.copyWith(
-                      color: AppColors.primary,
+              Gap(33),
+              BlocConsumer<SignInWithEmailCubit, SignInWithEmailState>(
+                listener: (context, state) {
+                  if (state is SignInWithEmailFailure) {
+                    snack(context, text: state.errMessage);
+                  } else if (state is SignInWithEmailSuccess) {
+                    snack(context, text: "تم ");
+                  }
+                },
+                builder: (context, state) {
+                  return state is SignInWithEmailLoading
+                      ? Center(
+                          child: CupertinoActivityIndicator(
+                            color: AppColors.primary,
+                            radius: 19,
+                          ),
+                        )
+                      : CustomButton(
+                          text: "تسجيل دخول",
+                          onTap: () {
+                            if (formKey.currentState!.validate()) {
+                              BlocProvider.of<SignInWithEmailCubit>(
+                                context,
+                              ).signInWithEmail(
+                                email: email.text,
+                                password: password.text,
+                              );
+                            }
+                          },
+                        );
+                },
+              ),
+              Gap(33),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "لا تمتلك حساب؟ ",
+                    style: AppStyles.semiBold16,
+                  ),
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pushNamed(SignUpView.id),
+                    child: Text(
+                      "قم بإنشاء حساب",
+                      style: AppStyles.semiBold16.copyWith(
+                        color: AppColors.primary,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Gap(33),
-            Row(
-              children: [
-                Expanded(child: Divider()),
-                Gap(18),
-                Text(
-                  "أو",
-                  style: AppStyles.semiBold16.copyWith(
-                    color: AppColors.textColor,
+                ],
+              ),
+              Gap(33),
+              Row(
+                children: [
+                  Expanded(child: Divider()),
+                  Gap(18),
+                  Text(
+                    "أو",
+                    style: AppStyles.semiBold16.copyWith(
+                      color: AppColors.textColor,
+                    ),
                   ),
-                ),
-                Gap(18),
+                  Gap(18),
 
-                Expanded(child: Divider()),
-              ],
-            ),
-            Gap(16),
-            CustomSocialAuthButton(
-              text: "تسجيل بواسطة جوجل",
-              icon: Assets.assetsImagesGoogleIcon,
-            ),
-            Gap(16),
-            CustomSocialAuthButton(
-              text: "تسجيل بواسطة أبل",
-              icon: Assets.assetsImagesApplIcon,
-            ),
-            Gap(16),
-            CustomSocialAuthButton(
-              text: "تسجيل بواسطة فيسبوك",
-              icon: Assets.assetsImagesFacebookIcon,
-            ),
-          ],
+                  Expanded(child: Divider()),
+                ],
+              ),
+              Gap(16),
+              CustomSocialAuthButton(
+                text: "تسجيل بواسطة جوجل",
+                icon: Assets.assetsImagesGoogleIcon,
+              ),
+              Gap(16),
+              CustomSocialAuthButton(
+                text: "تسجيل بواسطة أبل",
+                icon: Assets.assetsImagesApplIcon,
+              ),
+              Gap(16),
+              CustomSocialAuthButton(
+                text: "تسجيل بواسطة فيسبوك",
+                icon: Assets.assetsImagesFacebookIcon,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/services/shared_prefs.dart';
 import 'package:fruit_hub/core/utils/helper_functions/on_generate_route.dart';
+import 'package:fruit_hub/feaatures/auth/data/cubit/sign_in_with_email/sign_in_with_email_cubit.dart';
 import 'package:fruit_hub/feaatures/auth/data/cubit/sign_up_with_email/sign_up_with_email_cubit.dart';
 import 'package:fruit_hub/feaatures/auth/data/repos/auth_repo_impl.dart';
 import 'package:fruit_hub/feaatures/splash/presentation/views/splash_view.dart';
@@ -33,6 +34,10 @@ class FruitsApp extends StatelessWidget {
         BlocProvider(
           create: (BuildContext context) =>
               SignUpWithEmailCubit(AuthRepoImpl()),
+        ),
+        BlocProvider(
+          create: (BuildContext context) =>
+              SignInWithEmailCubit(AuthRepoImpl()),
         ),
       ],
       child: MaterialApp(
