8dfbb37e3d3ec2784d39aa0c2000df3b5ef8833c
Ibrahim Mohhamed <ibrahom0780@gmail.com>
2026-03-15 22:14:08 +0300

    create best selling grid view

--- lib/feaatures/home/presentation/widgets/best_selling_body.dart
+++ lib/feaatures/home/presentation/widgets/best_selling_body.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/feaatures/home/presentation/widgets/best_selling_grid_view.dart';
 
 class BestSellingBody extends StatelessWidget {
   const BestSellingBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: BestSellingGridView(),
+    );
   }
 }
---
+++ lib/feaatures/home/presentation/widgets/best_selling_grid_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/feaatures/home/presentation/widgets/fruit_item.dart';
+
+class BestSellingGridView extends StatelessWidget {
+  const BestSellingGridView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 20,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 163 / 233,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 16,
+      ),
+      itemBuilder: (context, index) {
+        return FruitItem();
+      },
+    );
+  }
+}
